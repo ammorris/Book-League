407fbd7aa20c821e95e9b2aa5799f5da8bff3717
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-09-21 21:03:58 -0700

    Got Google Books API hooked up

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
-    path("addbook/", views.AddBookView, name="addbook"),
+    path("addbook", views.AddBookView, name="addbook"),
+    path("searchResults/", views.SearchResultsView, name="searchResults"),
 ]
